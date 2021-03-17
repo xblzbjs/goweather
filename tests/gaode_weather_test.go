96711301775fcf6639df0bf166d3916fd1902ebf
xblzbjs <710255295@qq.com>
2021-03-17 16:02:21 +0800

    update gaode_weather_test.go

--- tests/gaode_weather_test.go
+++ tests/gaode_weather_test.go
@@ -29,9 +29,11 @@ func TestGetWeatherForCity(t *testing.T) {
 	if err != nil {
 		t.Log(err)
 	}
-	if res.Status != "1" {
+	if res.Status != "200 OK" {
 		t.Logf("GaoDeWeatherRequest Failed: %s", res.Status)
 	}
-	t.Log(weather)
-	t.Log(json.NewDecoder(res.Body).Decode(&weather))
+	t.Logf("今天的天气:%v", (weather.Forecasts[0].Casts[0]))
+	t.Logf("明天的天气:%v", weather.Forecasts[0].Casts[1])
+	t.Logf("后天的天气:%v", weather.Forecasts[0].Casts[2])
+	t.Logf("大后天的天气:%v", weather.Forecasts[0].Casts[3])
 }
